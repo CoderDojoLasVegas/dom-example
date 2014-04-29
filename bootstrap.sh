6ae01e183023968f360c7f843bab0269bb292e9e
Taylor Kaplan <taylor.kaplan@allegiantair.com>
2014-04-29 16:31:47 -0700

    Initial Commit

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+apt-get update
+apt-get install -y git
+apt-get install -y vim
+apt-get install -y python-software-properties
+apt-add-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
+npm update
+npm install -g phantomjs
+npm install -g casperjs
+npm install -g express
+npm install -g express-generator
+npm install -g bower
