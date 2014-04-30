c00c261605fa12777d900618fa7de3ad47b84f31
Taylor Kaplan <taylor.kaplan@allegiantair.com>
2014-04-30 09:07:13 -0700

    Basic JADE structure is done.

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 casper.test.begin('Localhost test', 0, function suite(test) {
   casper.start('localhost:3000', function() {
-    this.getElementsInfo('selector string');
+    this.getElementInfo('h1');
   });
 });
